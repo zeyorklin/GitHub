a43fee9abd23ea131fd6c961d061f102056d6472
zeyorklin <tonyzeyorklin@gmail.com>
2014-10-21 20:15:58 -0400

    oct21win

---
+++ src/SimpleGUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class SimpleGUI {
+	public static void main (String[] args)
+	{
+		JFrame frame = new JFrame();
+		JButton button = new JButton("Click me");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(button);
+		frame.setSize(300, 300);
+		frame.setVisible(true);
+	}
+}
