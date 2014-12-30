46cad422f13008097349df42fb8d9d34fbeebe70
zeyorklin <tonyzeyorklin@gmail.com>
2014-12-30 00:45:40 -0500

    Dec26Win

---
+++ network/src/Network.java
@@ -0,0 +1,7 @@
+
+public class Network {
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ network/src/TestSyncTest.java
@@ -0,0 +1,34 @@
+
+class TestSync implements Runnable {
+
+	private int balance;
+	
+	public void run() {
+		for(int i =0; i<50; i++)
+		{
+			increment();
+			
+		}
+		
+	}
+	
+	public synchronized void increment()
+	{
+		int i = balance;
+		balance = i+1;
+		System.out.println("balance is "+balance);
+	}
+
+}
+
+public class TestSyncTest {
+	public static void main(String[] args)
+	{
+		TestSync job = new TestSync();
+		Thread a = new Thread(job);
+		Thread b = new Thread(job);
+		a.start();
+		b.start();
+	}
+}
+
